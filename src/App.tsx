e222ac5f654f45fafd58f2ea6f0837fe7d89dabd
Zensu17 <sitorusalbert27@gmail.com>
2026-06-19 00:50:31 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1445,7 +1445,7 @@ const AppContent = () => {
                       </button>
                     ) : (
                       <div className="text-right text-[10px] font-bold text-slate-400 bg-slate-50 border border-slate-100 rounded-xl px-4 py-2.5 max-w-xs leading-relaxed">
-                        ⚠️ Hubungi Akademik jika terdapat ketidaksesuaian kelas.
+                        ⚠️ Hubungi Admin jika terdapat ketidaksesuaian kelas.
                       </div>
                     )}
                   </div>
